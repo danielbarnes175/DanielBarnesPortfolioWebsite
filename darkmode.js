515e8e8f5b5d26b164dfe2e4c7ecea90e2eee09f
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2019-09-11 20:26:39 -0500

    More testing on prod, don't tell the recruiters

--- darkmode.js
+++ darkmode.js
@@ -7,26 +7,28 @@ const grayBlack = "#000000";
 function init() {
 	console.log(sessionStorage.getItem('isDark'));
 	isDark = sessionStorage.getItem('isDark');
+	console.log("isDark " + isDark);
 	setColorOnLoad();
 };
+
 function toggleDarkMode() {
 	if (isDark) {
 		document.body.style.background = white;
 		document.body.style.color = black;
-
 		document.getElementById("iNeedThisToChangeTheHeaderWithJavascript").style.background = grayWhite;
 		isDark = false;
-		sessionStorage.setItem('isDark', 'false');
 	} else {
 		document.body.style.background = black;
 		document.body.style.color = white;
 		document.getElementById("iNeedThisToChangeTheHeaderWithJavascript").style.background = grayBlack;
 		isDark = true;
-		sessionStorage.setItem('isDark', 'true');
 	}
+	sessionStorage.setItem('isDark', isDark);
+	console.log("Changed isDark:" + isDark);
 };
+
 function setColorOnLoad() {
-	if (!isDark) {
+	if (isDark) {
 		document.body.style.background = white;
 		document.body.style.color = black;
 		document.getElementById("iNeedThisToChangeTheHeaderWithJavascript").style.background = grayWhite;
