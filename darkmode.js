56728a6abce57f473186dfd703915a51437497ed
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2019-09-11 20:23:17 -0500

    Need to test in production hmmmm

--- darkmode.js
+++ darkmode.js
@@ -5,6 +5,7 @@ const grayWhite = "#E6E6E6";
 const grayBlack = "#000000";
 
 function init() {
+	console.log(sessionStorage.getItem('isDark'));
 	isDark = sessionStorage.getItem('isDark');
 	setColorOnLoad();
 };
@@ -29,11 +30,9 @@ function setColorOnLoad() {
 		document.body.style.background = white;
 		document.body.style.color = black;
 		document.getElementById("iNeedThisToChangeTheHeaderWithJavascript").style.background = grayWhite;
-		sessionStorage.setItem('isDark', 'false');
 	} else {
 		document.body.style.background = black;
 		document.body.style.color = white;
 		document.getElementById("iNeedThisToChangeTheHeaderWithJavascript").style.background = grayBlack;
-		sessionStorage.setItem('isDark', 'true');
 	}
 }
